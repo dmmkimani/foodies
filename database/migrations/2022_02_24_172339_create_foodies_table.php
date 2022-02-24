93ce702ccebce50c3caf5f4da44667e8092f936d
MikeMwaura <41745528+MikeMwaura@users.noreply.github.com>
2022-02-24 18:20:06 +0000

    Created the foodie migration

---
+++ database/migrations/2022_02_24_172339_create_foodies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foodies', function (Blueprint $table) {
+            $table->string('username')->unique();
+            $table->string('cover_image')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->string('fName')->nullable();
+            $table->string('lName')->nullable();
+            $table->dateTime('date_registered')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foodies');
+    }
+};
